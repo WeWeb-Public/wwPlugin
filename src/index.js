02c8b861ef501f5525a1c0dcd12c2cd9767e935a
Kramerbaf <nds.kram@gmail.com>
2019-03-12 14:21:43 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import plugin from './plugin.js'
+
+const name = "__NAME__";
+const version = '__VERSION__';
+
+wwLib.wwPlugins.add(name, plugin.init)
---
+++ src/plugin.js
@@ -0,0 +1,6 @@
+
+export default {
+    init() {
+        console.log('Init my plugin');
+    }
+}
